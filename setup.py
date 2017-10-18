3b1c26d1962e6a14ce9b95dd3e1b73f3b07ccf46
Gunnar Aastrand Grimnes <gromgull@gmail.com>
2017-10-18 15:19:37 +0200

    added real version number

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python
 
+import re
+import io
+import os.path
+
+
 from setuptools import setup
 
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 setup(name='bbpyutils',
-      version='0.1',
+      version=find_version('bb', '__init__.py'),
       description='Various internal Bakken Baeck utilities',
       packages=['bb'],
       install_requires = []
